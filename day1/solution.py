efda0800a348283cc6bfac13b6c775d43b9e2e55
Anuraj Tapidas <anutapid@publicisgroupe.net>
2021-12-02 16:57:43 +0000

    multiplication optimization

--- day1/solution.py
+++ day1/solution.py
@@ -22,7 +22,7 @@ reduce(func, enumerate(input), 0)
 # not-suffed solution part 1
 total = 0
 for index in range(1, len(input)):
-    total += 1 if input[index] > input[index-1] else 0
+    total += 1 * (input[index] > input[index-1])
 total
 # %%
 # part 2
@@ -30,7 +30,7 @@ total = 0
 for i in range(2, len(input)-1):
     window1 = sum(input[i-3:i])
     window2 = sum(input[i-2:i+1])
-    total += 1 if window2 > window1 else 0
+    total += 1 * (window2 > window1)
 total
 
 # %% part 1 in 1 line
