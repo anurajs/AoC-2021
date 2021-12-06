8bbb852cacc57cd6e3f969f2252492e00fbe7989
Anuraj Tapidas <anutapid@publicisgroupe.net>
2021-12-06 09:59:55 +0000

    big day 6

---
+++ day6/solution.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+path = 'puzzle.txt'
+with open(path, 'r') as file:
+    input = file.readline()
+
+fishes = defaultdict(lambda: 0)
+for key in [int(x) for x in input.split(',')]:
+    fishes[key] += 1
+
+for _ in range(256):
+    for i in range(9):
+        amount = fishes[i]
+        fishes[i-1] += fishes[i]
+        fishes[i] = 0
+    born = fishes[-1]
+    fishes[-1] = 0
+    fishes[6] += born
+    fishes[8] += born
+
+
+print(sum(fishes.values()))
