851d2a99bcb0a6c9475f9a709c205e23b51a0e18
Anuraj Tapidas <anutapid@publicisgroupe.net>
2021-12-07 10:23:38 +0000

    day 7

---
+++ day7/solution.py
@@ -0,0 +1,27 @@
+from statistics import median
+
+path = 'puzzle.txt'
+with open(path, 'r') as file:
+    input = file.readline()
+
+input = [int(x) for x in input.split(',')]
+med = median(input)
+total = 0
+for crab in input:
+    total += abs(crab - med)
+
+print('Part 1: ', total)
+
+
+def consec_num(x):
+    return (x/2 * (1+x))
+
+
+average = sum(input)//len(input)
+
+
+total = 0
+for crab in input:
+    total += consec_num(abs(crab - average))
+
+print('Part 2: ', total)
