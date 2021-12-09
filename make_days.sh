af5bac941f010bab1f6cf5444aac46bffba58b9b
Anuraj Tapidas <anutapid@publicisgroupe.net>
2021-12-09 14:30:25 +0000

    added making solution files to script

--- make_days.sh
+++ make_days.sh
@@ -3,6 +3,7 @@ for ((i = 1 ; i < 26 ; i++)); do
   x="`printf \"%02d\" $i`"
   if [[ ! (-e day$x)]]; then
     mkdir day$x
+    touch day$x/solution.py
     touch day$x/input.txt
     touch day$x/puzzle.txt
   fi
