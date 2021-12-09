91c3e95def2f9ce3f025f13d9e0489b9e7e37250
Anuraj Tapidas <anutapid@publicisgroupe.net>
2021-12-09 14:26:59 +0000

    fixed make script bug

--- make_days.sh
+++ make_days.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-for ((i = 1 ; i < 26 ; i++)); dogit 
+for ((i = 1 ; i < 26 ; i++)); do
+  x="`printf \"%02d\" $i`"
   if [[ ! (-e day$x)]]; then
     mkdir day$x
     touch day$x/input.txt
