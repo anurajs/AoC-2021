14a5bcbb411ef5dc6baa2476100ae0fd2d48ea42
Anuraj Tapidas <atapidas@gmail.com>
2021-12-12 13:54:07 +0000

    day 12

---
+++ day12/solution.py
@@ -0,0 +1,45 @@
+path = 'puzzle.txt'
+with open(path) as file:
+    data = file.readlines()
+
+
+def traverse(node, path, edgeList, paths, visited, smalled):
+    if node == 'end':
+        paths.append(path)
+        return
+    for con in edgeList[node]:
+        small = smalled
+        if (con == 'start'):
+            continue
+        if (con in visited and con.islower()):
+            if smalled:
+                continue
+            else:
+                small = True
+        new_path = path.copy()
+        new_path.append(con)
+        new_visited = visited.copy()
+        new_visited.add(con)
+        traverse(con, new_path, edgeList, paths, new_visited, small)
+
+
+edgeList = dict()
+for edges in data:
+    edges = edges.strip().split('-')
+    start, end = edges
+    if start not in edgeList:
+        edgeList[start] = {end}
+    else:
+        edgeList[start].add(end)
+    if end not in edgeList:
+        edgeList[end] = {start}
+    else:
+        edgeList[end].add(start)
+
+paths1 = []
+paths2 = []
+traverse('start', ['start'], edgeList, paths1, {'start'}, True)
+traverse('start', ['start'], edgeList, paths2, {'start'}, False)
+
+print('Part 1: ', len(paths1))
+print('Part 2: ', len(paths2))
