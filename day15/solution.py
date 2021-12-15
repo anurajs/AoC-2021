de35c30c72fbc9dcf45adb7263ff0a1153ea214a
Anuraj Tapidas <atapidas@gmail.com>
2021-12-15 15:52:32 +0000

    removed part 1 code

--- day15/solution.py
+++ day15/solution.py
@@ -8,41 +8,12 @@ with open(path) as file:
 data = [[int(x) for x in line.strip()] for line in data]
 
 
-def part1(data):
-    start = (0, 0)
-    end = (len(data[0])-1, len(data)-1)
-    current = start
-
-    distances = defaultdict(lambda: float('inf'))
-    previous = dict()
-    distances[start] = 0
-    available = []
-    visited = set()
-    while current != end:
-        visited.add(current)
-        neighbours = [(current[0] + 1, current[1]), (current[0]-1, current[1]),
-                      (current[0], current[1] + 1), (current[0], current[1]-1)]
-        for (x, y) in neighbours:
-            if not(y == len(data) or x == len(data[y]) or x < 0 or y < 0) and (x, y) not in visited:
-                alt = distances[current] + data[y][x]
-                if alt < distances[(x, y)]:
-                    distances[(x, y)] = alt
-                    previous[(x, y)] = current
-                    heapq.heappush(available, (alt, (x, y)))
-        _, current = heapq.heappop(available)
-        while current in visited:
-            _, current = heapq.heappop(available)
-
-    return distances[end]
-
-
 def get_dist(data, x, y):
     return (data[y % len(data)][x % len(data[0])] + (x//len(data[0])) + (y//len(data)) - 1) % 9 + 1
 
 
-def part2(data):
+def solve(data, end):
     start = (0, 0)
-    end = (len(data[0]) * 5 - 1, len(data) * 5 - 1)
     current = start
     distances = defaultdict(lambda: float('inf'))
     previous = dict()
@@ -67,5 +38,5 @@ def part2(data):
     return distances[end]
 
 
-print('Part 1: ', part1(data))
-print('Part 2: ', part2(data))
+print('Part 1: ', solve(data, (len(data[0]) - 1, len(data) - 1)))
+print('Part 2: ', solve(data, (len(data[0]) * 5 - 1, len(data) * 5 - 1)))
